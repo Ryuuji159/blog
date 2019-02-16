325659d1f5022207b9ebb6690081f85002132f2c
Daniel Cortes <daniel.cortes.p159@gmail.com>
2019-02-16 03:10:57 -0300

    Creado un "panel" de administracion, basicamente son links a las rutas para crear cosas :3

--- www/admin.py
+++ www/admin.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 
 from www.db import get_db
+from www.auth import admin_required
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 @bp.route('/panel')
+@admin_required
 def panel():
     return render_template('admin/panel.html')
