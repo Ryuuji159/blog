2f09a63357bec82fc1abe292be9f2c67ac2cbd4d
Daniel Cortés <daniel.cortes.p159@gmail.com>
2019-06-16 17:37:08 -0400

    Siguiendo un tuto de vue <3

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -3,12 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Spa!</title>
     </head>
     <body>
         <div id="app">
-            <example-component></example-component>
+            <app></app>
         </div>
+
         <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
