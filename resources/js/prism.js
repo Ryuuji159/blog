00e8af6a429b979fc556d814c7471944a1e3c6d1
Daniel Cortés <daniel.cortes.p159@gmail.com>
2019-10-23 00:55:51 -0300

    feat: Mas lenguajes para prism
    El ultimo pos que puse necesita highlights en bash y el que voy a hacer
    ahora usara sql, asi que ahi va

--- resources/js/prism.js
+++ resources/js/prism.js
@@ -4,5 +4,7 @@ require("prismjs/components/prism-java.js");
 require("prismjs/components/prism-php.js");
 require("prismjs/components/prism-php-extras.js");
 require("prismjs/components/prism-markup-templating.js");
+require("prismjs/components/prism-bash.js");
+require("prismjs/components/prism-sql.js");
 
 prismjs.highlightAll();
